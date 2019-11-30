7a70865e7defcdb636b72c69ad744fd23f209ca3
CPiparo95 <claudiodpiparo@gmail.com>
2019-11-30 17:52:04 -0300

    dejando solo batalla naval

--- Javascript Development/Task 1/test.js
+++
@@ -1,8 +0,0 @@
-function mayus(texto) {
-    var perro = texto.charAt(texto.length-1);
-    //return texto.charAt(0).toUpperCase() + string.slice(1);
-}
-
-//ejer2
-var textoPrueba = ("chicho es un perro bueno");
-console.log(mayus(textoPrueba));
